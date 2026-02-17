84a6654ec9697cb8c2f0f02229b309c99de3a120
GrugLife <gwylie1@gmail.com>
2026-02-16 16:05:25 -0800

    finish blackjack

--- blackjack.py
+++ blackjack.py
@@ -11,8 +11,11 @@ def hit_stay(player):
             player.append(random.choice(cards))
             print(player)
             if sum(player) > 21:
-                print("bust, you lose")
-                return "BUST!"
+                if 11 in player:
+                    player.remove(11)
+                    player.append(1)
+                else:
+                    return sum(player)
 
     return sum(player)
 
@@ -20,7 +23,7 @@ def hit_stay(player):
 def dealer_draw(dealer):
     while sum(dealer) < 17:
         dealer.append(random.choice(cards))
-    print(dealer)
+        print(dealer)
     return sum(dealer)
 
 
@@ -34,41 +37,39 @@ def won(player, dealer):
 
 
 def deal(cards):
-    # 1. deal cards to player and dealer
-
     player_cards = []
     dealer_cards = []
 
-    n = 0
-    while n < 4:
+    for n in range(2):
         player_cards.append(random.choice(cards))
         dealer_cards.append(random.choice(cards))
-        n += 2
 
-    # 1.5 show cards after deal
-    print(player_cards)
-    print(dealer_cards[0])
+    print(f"Your hand:   {player_cards}")
+    print(f"Dealer hand: {dealer_cards[0]}")
+
+    return player_cards, dealer_cards
+
+
+def main():
+    player_cards, dealer_cards = deal(cards)
+    if sum(player_cards) == 21:
+        return "You win with a blackjack"
+    elif sum(dealer_cards) == 21:
+        print(dealer_cards)
+        return "Dealer wins with a blackjack"
 
     player_score = hit_stay(player_cards)
+    if player_score > 21:
+        return f"You bust with a {player_score}"
+
+    print(f"Dealer is showing: {dealer_cards}")
     dealer_score = dealer_draw(dealer_cards)
+    if dealer_score > 21:
+        return f"You win, dealer bust with a {dealer_score}"
     winner = won(player_score, dealer_score)
 
-    print(winner)
+    return winner
 
 
-deal(cards)
-
-
-# 2. calc score for player
-
-
-# 3. ask to hit or stay
-
-
-# 4. if hit, deal another card and cal score
-
-# 5. if stay, move to dealers score
-
-# 6. if dealer <17, deal a card
-
-# 7. see who wins
+while input("Do you want to play a game of Blackjack? Type 'y' or 'no': ") == "y":
+    print(main())
